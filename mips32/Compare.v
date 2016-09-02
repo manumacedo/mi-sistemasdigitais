a0ae137d5eb5399f8b00bde2f89538094466c23f
Admin <pedroecomp@gmail.com>
2016-09-02 09:53:05 -0300

    Modulos : Add,Compare,Extender,Mux2,Mux4

---
+++ mips32/Compare.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+module Compare(
+    input  [31:0] A,
+    input  [31:0] B,
+    output EQ, #
+    output TLZ, #
+    output NE, #
+    output GEZ, #
+
+    );
+
+    assign EQ  = ( A == B);
+    assing NE =  ( A != B);
+    assign TLZ  = ( A[31] < 32'b0 );
+    assign GEZ =  ~A[31];
+
+
+endmodule
---
+++ mips32/Mux2.v
@@ -0,0 +1,11 @@
+`timescale 1ns / 1ps
+
+module Mux2 #(parameter WIDTH = 32)(
+    input  sel,
+    input  [(WIDTH-1):0] in0, in1,
+    output [(WIDTH-1):0] out
+    );
+
+    assign out = (sel) ? in1 : in0;
+
+endmodule
