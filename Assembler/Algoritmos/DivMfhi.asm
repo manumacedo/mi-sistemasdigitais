40f67a755c9d7020653b8da8826616986dfe5de2
Pedro Cardoso <pedroecomp@gmail.com>
2016-10-15 14:48:26 -0300

    teste Div

--- Assembler/Algoritmos/DivMfhi.asm
+++ Assembler/Algoritmos/DivMfhi.asm
@@ -1,7 +1,13 @@
 # Bloco 3: DIV, MFHI
+
 .text
-lui $t0, 10
-lui $t1, 2
-lui $t2, 200
-Div $t0, $t1
-Div $t2, $t1
+  lui $t0, 10
+  lui $t1, 2
+  lui $t2, 200
+  div $t0, $t1
+  mfhi $s2
+  sw $s2, 16400
+  addi $s1, $t1, 8
+  div $t2, $s1
+  mfhi $s4
+  sw $s4, 16400
